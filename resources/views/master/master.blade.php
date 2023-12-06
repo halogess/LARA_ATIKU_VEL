4682c5faa34d92f1cda2c16953cf4c921eff1f34
hanuy04 <108392293+hanuy04@users.noreply.github.com>
2023-12-06 14:30:03 +0700

    update

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -5,7 +5,7 @@
         <div class="topBarang mt-5 ml-72 p-5 mr-10 border-black bg-gray-400 w-60 h-96 float-left">
             <p class="text-dark text-center text-xl">- Top 10 Transaksi -</p>
         </div>
-        <div class="topAdmin mt-5 mr-10 p-5 border-black bg-gray-400 w-60 h-96 float-left">
+        <div class="topPembeli mt-5 mr-10 p-5 border-black bg-gray-400 w-60 h-96 float-left">
             <p class="text-dark text-center text-xl">- Top 10 Pembeli -</p>
         </div>
         <div class="income mt-5 mr-10 p-5 border-black bg-gray-400 w-60 h-96 float-left">
