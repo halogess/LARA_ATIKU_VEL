844c09d82e8355c06858367b43900e80631fd32f
hanuy04 <108392293+hanuy04@users.noreply.github.com>
2023-12-06 13:43:03 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,32 +2,32 @@
 
 @section('content')
     <div class="content h-auto">
-        <div id="animation-carousel" class="relative w-full mt-5" data-carousel="static">
+        <div id="animation-carousel" class="relative w-full" data-carousel="static">
             <!-- Carousel wrapper -->
             <div class="relative h-56 overflow-hidden md:h-96">
                 <!-- Item 1 -->
                 <div class="duration-200 ease-linear" data-carousel-item>
-                    <img src="{{ asset('porschetaycan.jpg') }}"
+                    <img src="{{ asset('/img/porschetaycan.jpg') }}"
                         class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                 </div>
                 <!-- Item 2 -->
                 <div class="duration-200 ease-linear" data-carousel-item>
-                    <img src="{{ asset('fordranger.jpg') }}"
+                    <img src="{{ asset('/img/fordranger.jpg') }}"
                         class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                 </div>
                 <!-- Item 3 -->
                 <div class="duration-200 ease-linear" data-carousel-item="active">
-                    <img src="{{ asset('wulingairev.jpg') }}"
+                    <img src="{{ asset('/img/wulingairev.jpg') }}"
                         class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                 </div>
                 <!-- Item 4 -->
                 <div class="duration-200 ease-linear" data-carousel-item>
-                    <img src="{{ asset('kiaev9.jpg') }}"
+                    <img src="{{ asset('/img/kiaev9.jpg') }}"
                         class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                 </div>
                 <!-- Item 5 -->
                 <div class="duration-200 ease-linear" data-carousel-item>
-                    <img src="{{ asset('chevroletcamaro.jpg') }}"
+                    <img src="{{ asset('/img/chevroletcamaro.jpg') }}"
                         class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                 </div>
             </div>
@@ -62,20 +62,22 @@
 
         {{-- menu awal --}}
         <div class="menu mt-20 ml-32 w-fit h-auto">
-            <div class="unitmobil text-black float-left mr-5 w-64 h-40"
-                style="background-image: url('{{ asset('unitmobil.jpg') }}'); background-repeat: no-repeat; background-size: cover;">
+            <p class="text-center text-4xl">Products</p>
+            <hr class="border-black">
+            <div class="unitmobil text-black float-left ml-3 mr-5 mt-5 w-64 h-40"
+                style="background-image: url('{{ asset('/img/unitmobil.jpg') }}'); background-repeat: no-repeat; background-size: cover;">
                 <b class="bottom-0 right-0 text-white text-right text-2xl">Unit</b>
             </div>
-            <div class="spareparts text-black float-left mr-5 w-64 h-40"
-                style="background-image: url('{{ asset('spareparts.jpg') }}'); background-repeat: no-repeat; background-size: cover;">
+            <div class="spareparts text-black float-left mr-5 mt-5 w-64 h-40"
+                style="background-image: url('{{ asset('/img/spareparts.jpg') }}'); background-repeat: no-repeat; background-size: cover;">
                 <b class="bottom-0 right-0 text-white text-right text-2xl">Spareparts</b>
             </div>
-            <div class="aftermarket text-black float-left mr-5 w-64 h-40"
-                style="background-image: url('{{ asset('aftermarket.jpg') }}'); background-repeat: no-repeat; background-size: cover;">
+            <div class="aftermarket text-black float-left mr-5 mt-5 w-64 h-40"
+                style="background-image: url('{{ asset('/img/aftermarket.jpg') }}'); background-repeat: no-repeat; background-size: cover;">
                 <b class="bottom-0 right-0 text-white text-right text-2xl">Aftermarket</b>
             </div>
-            <div class="interior text-black float-right mr-5 w-64 h-40"
-                style="background-image: url('{{ asset('interior.jpg') }}'); background-repeat: no-repeat; background-size: cover;">
+            <div class="interior text-black float-right mr-5 mt-5 w-64 h-40"
+                style="background-image: url('{{ asset('/img/interior.jpg') }}'); background-repeat: no-repeat; background-size: cover;">
                 <b class="bottom-0 right-0 text-white text-right text-2xl">Interior</b>
             </div>
         </div>
@@ -84,22 +86,22 @@
         <div class="judul text-center mt-80 text-black w-auto h-auto">
             <p class="text-4xl">Why Choose Us?</p>
         </div>
-        <div class="alasan1 relative text-right w-full h-96 p-5 overflow-hidden">
+        <div class="alasan1 relative text-right w-full h-96 p-5 overflow-hidden mt-6">
             <div class="blur-background absolute inset-0 w-full h-full"
-                style="background-image: url('{{ asset('trusted.jpg') }}'); background-repeat: no-repeat; background-size: cover; filter: blur(4px); -webkit-filter: blur(8px);">
+                style="background-image: url('{{ asset('/img/trusted.jpg') }}'); background-repeat: no-repeat; background-size: cover; filter: blur(8px); -webkit-filter: blur(8px);">
             </div>
-            <div class="text-content relative text-white z-10">
-                <p class="text-7xl mr-32 mt-32">Trusted</p>
+            <div class="text-content relative text-black z-10">
+                <p class="text-7xl mr-32 mt-20">Trusted</p>
                 <p class="text-4xl mr-32">You can consult with admins who already have high experience. Don't be late
                     to check out our products!</p>
             </div>
         </div>
         <div class="alasan2 relative text-left w-full h-96 p-5 overflow-hidden">
             <div class="blur-background absolute inset-0 w-full h-full"
-                style="background-image: url('{{ asset('secure.jpg') }}'); background-repeat: no-repeat; background-size: cover; filter: blur(4px); -webkit-filter: blur(8px);">
+                style="background-image: url('{{ asset('/img/secure.jpg') }}'); background-repeat: no-repeat; background-size: cover; filter: blur(8px); -webkit-filter: blur(8px);">
             </div>
-            <div class="text-content relative text-white z-10">
-                <p class="text-7xl ml-32 mt-32">Secure</p>
+            <div class="text-content relative text-black z-10">
+                <p class="text-7xl ml-32 mt-20">Secure</p>
                 <p class="text-4xl ml-32">All transactions carried out are protected by insurance that our company has and
                     has verified!</p>
             </div>
@@ -107,10 +109,10 @@
     </div>
     <div class="alasan3 relative text-right w-full h-96 p-5 overflow-hidden">
         <div class="blur-background absolute inset-0 w-full h-full"
-            style="background-image: url('{{ asset('express.jpeg') }}'); background-repeat: no-repeat; background-size: cover; filter: blur(4px); -webkit-filter: blur(8px);">
+            style="background-image: url('{{ asset('/img/express.jpeg') }}'); background-repeat: no-repeat; background-size: cover; filter: blur(8px); -webkit-filter: blur(8px);">
         </div>
-        <div class="text-content relative text-white z-10">
-            <p class="text-7xl mr-32 mt-32">Express</p>
+        <div class="text-content relative text-black z-10">
+            <p class="text-7xl mr-32 mt-20">Express</p>
             <p class="text-4xl mr-32">Checking, transactions and shipping are sent by fast and trusted agents. You
                 won't be late trying!</p>
         </div>
