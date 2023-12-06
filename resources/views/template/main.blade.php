b0ea7113e4f9d415071edc3602902220cadc92e6
hanuy04 <108392293+hanuy04@users.noreply.github.com>
2023-12-06 10:27:03 +0700

    update

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -6,12 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="/style/cdn.tailwindcss.com_3.3.3"></script>
 </head>
 
 <body>
-    @yield('navbar')
+    <nav class="w-full h-12 bg-white text-dark">
+        <div class="judul h-auto w-auto ml-24 mt-4 float-left">
+            <p class="font-bold">JJHC Automotive</p>
+        </div>
+        <div class="buttons h-auto w-auto mr-24 mt-4 float-right">
+            <form action="" method="post">
+                <input type="submit" value="Log In" class="mr-5 w-16 h-auto bg-gray-400 text-black p-1"><input
+                    type="submit" value="Sign Up" class="w-16 h-auto bg-gray-400 text-black p-1">
+            </form>
+        </div>
+    </nav>
     @yield('content')
 </body>
 
